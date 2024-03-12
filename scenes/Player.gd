a70ae99a646ddd2fd04cd39bc8e017251aad5990
azuka-p <rad.azka.16@gmail.com>
2024-03-12 23:15:45 +0700

    Run gdformat

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,48 +1,54 @@
 extends KinematicBody2D
 
-export (int) var speed = 400
-export (int) var jump_speed = 600
-export (int) var GRAVITY = 1200
+export(int) var speed = 400
+export(int) var jump_speed = 600
+export(int) var GRAVITY = 1200
 
-const UP = Vector2(0,-1)
+const UP = Vector2(0, -1)
 
 var velocity = Vector2()
 
 var jump_count: int = 0
 const max_jumps: int = 2
 
+
 func jump():
 	if jump_count < max_jumps:
 		jump_count += 1
 		velocity.y = -jump_speed
 
+
 var dash_count_r: int = 0
 var dashing_r: bool = false
 
+
 func dash_right():
-	if Input.is_action_just_pressed('right') and dashing_r == false and dash_count_r == 1:
+	if Input.is_action_just_pressed("right") and dashing_r == false and dash_count_r == 1:
 		dashing_r = true
 		velocity.x += 4000
-		yield(get_tree().create_timer(.5),"timeout")
+		yield(get_tree().create_timer(.5), "timeout")
 		dashing_r = false
-	if Input.is_action_just_pressed('right') and dashing_r == false:
+	if Input.is_action_just_pressed("right") and dashing_r == false:
 		dash_count_r += 1
-		yield(get_tree().create_timer(.2),"timeout")
+		yield(get_tree().create_timer(.2), "timeout")
 		dash_count_r = 0
+
 
 var dash_count_l: int = 0
 var dashing_l: bool = false
 
+
 func dash_left():
-	if Input.is_action_just_pressed('left') and dashing_l == false and dash_count_l == 1:
+	if Input.is_action_just_pressed("left") and dashing_l == false and dash_count_l == 1:
 		dashing_l = true
 		velocity.x -= 4000
-		yield(get_tree().create_timer(.5),"timeout")
+		yield(get_tree().create_timer(.5), "timeout")
 		dashing_l = false
-	if Input.is_action_just_pressed('left') and dashing_l == false:
+	if Input.is_action_just_pressed("left") and dashing_l == false:
 		dash_count_l += 1
-		yield(get_tree().create_timer(.2),"timeout")
+		yield(get_tree().create_timer(.2), "timeout")
 		dash_count_l = 0
+
 
 func get_input():
 	velocity.x = 0
@@ -66,12 +72,13 @@ func get_input():
 	if velocity.x == 0 and velocity.y == 0:
 		$AnimatedSprite.play("idle")
 
+
 func _physics_process(delta):
 	get_input()
 	velocity.y += delta * GRAVITY
-	
+
 	velocity = move_and_slide(velocity, UP)
-	
+
 	var bite := false
 	for index in get_slide_count():
 		var collision := get_slide_collision(index)
@@ -80,6 +87,6 @@ func _physics_process(delta):
 			bite = true
 	if bite:
 		$bite.play()
-	
+
 	if is_on_floor():
 		jump_count = 0
--- scenes/tutorial-5/T5Player.gd
+++ scenes/tutorial-5/T5Player.gd
@@ -1,24 +1,25 @@
 extends KinematicBody2D
 
-export (int) var speed = 400
-export (int) var jump_speed = -600
-export (int) var GRAVITY = 1200
+export(int) var speed = 400
+export(int) var jump_speed = -600
+export(int) var GRAVITY = 1200
 
-const UP = Vector2(0,-1)
+const UP = Vector2(0, -1)
 
 var velocity = Vector2()
+
 
 func get_input():
 	var animation = "diri_kanan"
 
 	velocity.x = 0
-	if is_on_floor() and Input.is_action_just_pressed('up'):
+	if is_on_floor() and Input.is_action_just_pressed("up"):
 		velocity.y = jump_speed
-	if Input.is_action_pressed('right'):
+	if Input.is_action_pressed("right"):
 		velocity.x += speed
 		animation = "jalan_kanan"
 		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed('left'):
+	if Input.is_action_pressed("left"):
 		velocity.x -= speed
 		animation = "jalan_kanan"
 		$AnimatedSprite.flip_h = true
@@ -26,11 +27,12 @@ func get_input():
 	if $AnimatedSprite.animation != animation:
 		$AnimatedSprite.play(animation)
 
+
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	get_input()
 	velocity = move_and_slide(velocity, UP)
-	
+
 	var ded := false
 	for index in get_slide_count():
 		var collision := get_slide_collision(index)
